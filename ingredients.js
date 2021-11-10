60e4340ad431077f237e2d75263d807435308f34
Paules2021 <P.elishaee@gmail.com>
2021-11-10 19:54:19 +0000

    print out array by loop and backward printing

---
+++ ingredients.js
@@ -0,0 +1,14 @@
+let i=0
+while(i<ingredients.length){
+    console.log(ingredients[i])
+    i++
+}
+// Write a for loop that prints out the contents of ingredients:
+for( let i=0; i<ingredients.length ;i++){
+    console.log(ingredients[i])
+}
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+ingredients.reverse()
+for( let i=0; i<ingredients.length ;i++){
+    console.log(ingredients[i])
+}
